b87c4e8aa817b1f5d4c27eef997bdf6274cfc475
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-03 14:48:19 +0300

    updated

--- src/pages/MyGamesLibrary/MyGamesLibraryPage.jsx
+++ src/pages/MyGamesLibrary/MyGamesLibraryPage.jsx
@@ -9,11 +9,7 @@ import { authActions } from "store/auth";
 import { toast } from "react-toastify";
 import "../MyGamesLibrary/MyGamesLibrary.scss";
 
-//* useSelector is used to get the loggedIn and userInfo state from the Redux store.
-//* useState is used to initialize two variables: gameCardArr and filterInput.
-//* useLocation is used to get the current location object from React Router.
-//* useHistory is used to navigate between pages using the history.push() method.
-//* useDispatch is used to dispatch actions to the Redux store.
+
 const LibraryPage = () => {
   const loggedIn = useSelector((state) => state.auth.loggedIn);
   const userInfo = useSelector((state) => state.auth.userInfo);
@@ -24,24 +20,12 @@ const LibraryPage = () => {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  //* the code inside the callback function checks if the userInfo variable is truthy and if it has a wishList property.
-  //* If both of those conditions are true, it then calls setGameCardArr to update the component's gameCardArr state variable with the value of (userInfo.wishList).
-  //*In other words, this useEffect is to set the gameCardArr to the wishlist of the user when userInfo is updated.
+  
   useEffect(() => {
     if (userInfo && userInfo.wishList) setGameCardArr(userInfo.wishList);
   }, [userInfo]);
 
-  //* the useEffect first checks that the userInfo and (userInfo.wishList) are truthy.
-  //* If they are, it creates a new URLSearchParams object using the (location.search value).
-  //* Then it checks if the URLSearchParams object has a "filter" parameter.
-  //* If it does, it assigns the value of that parameter to the filter variable and creates a regular expression object that matches the filter input in a case-insensitive way.
-  //* It then creates a new array newFilteredArr that is a deep copy of (userInfo.wishList) and filters this array based on the regular expression filter.
-  //* then, it compares filter to filterInput and if they are different, it updates the state using setFilterInput(filter).
-  //* then is checking if the userInfo and userInfo.wishList objects exist.
-  //* If they do, it then creates a new URLSearchParams object based on the current location, which is used to check if the query string contains a "filter" or "sort" parameter.
-  //* If the "filter" parameter is present, it sets a newFilteredArr variable to a filtered version of (userInfo.wishList), where each game's gameName property is matched against the "filter" parameter using a regular expression.
-  //* If the "sort" parameter is present, it sorts newFilteredArr in ascending or descending order based on the value of "sort" parameter.
-  //* Finally, if newFilteredArr is defined, it sets the state of gameCardArr to the newFilteredArr, which will trigger a re-render of the component and update the view with the filtered and sorted data.
+
   useEffect(() => {
     if (userInfo && userInfo.wishList) {
       const qParmas = new URLSearchParams(location.search);
@@ -99,10 +83,7 @@ const LibraryPage = () => {
     }
   }, [location, userInfo]);
 
-  //* The function checks if the key pressed is the Enter key.
-  //* If it is, the function creates a new URLSearchParams object using the current location's search property.
-  //* It then sets the "filter" parameter to the current value of the filterInput state.
-  //* It then updates the browser's URL by calling the push method on the history object, passing in a string with the new URL, including the updated query parameters.
+  
   const handleInputKeyUp = (ev) => {
     if (ev.code === "Enter") {
       let qParmas = new URLSearchParams(location.search);
@@ -111,62 +92,37 @@ const LibraryPage = () => {
     }
   };
 
-  //* This is a JavaScript function called handleCategoryFilter that takes an event object as an argument.
-  //*The function is used to update a state variable called categoryFilter with the value of the selected option from a dropdown menu.
-  //* The ev argument is an event object that represents the user's interaction with the dropdown menu.
-  //* When the user selects an option from the menu, the value property of the selected option is used to update the categoryFilter state variable.
-  //* The setCategoryFilter function is called with the value of the selected option as an argument.
-  //* This function is used to update the state variable categoryFilter with the new value.
-  //* By updating the categoryFilter state variable, the function will trigger a re-render of the component,
-  //* which will update the content displayed on the page based on the selected category.
+  
   const handleCategoryFilter = (ev) => {
     setCategoryFilter(ev.target.value);
   };
 
-  //* This is a React useEffect hook that triggers a re-render whenever the categoryFilter state variable changes.
-  //* The effect updates the URL query string with the new category filter value, which in turn updates the content displayed on the page.
-  //* Here's how it works:
-  //* The useEffect hook is called whenever the categoryFilter state variable changes.
-  //* It creates a new instance of the URLSearchParams object, which parses the current query string in the URL.
-  //* It then sets the category parameter of the qParams object with the new value of categoryFilter.
-  //* Finally, it calls the history.push function to update the URL with the new query string.
-  //* By updating the query string in the URL, the component can use the new value of categoryFilter to filter and display the correct content on the page.
+  
   useEffect(() => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("category", categoryFilter);
     history.push(`/librarypage?${qParmas.toString()}`);
   }, [categoryFilter]);
 
-  //* ascending sort button #1.
-  //* The function retrieves the current search parameters from the browser's URL using the URLSearchParams object, sets the "sort" parameter to "asc" and updates the URL using the (history.push) method,
-  //* so the page will navigate to the (/librarypage) path with the updated parameters.
-  //* This allows the user to sort the games in the library in ascending order.
+  
   const handleSortASCClick = () => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("sort", "asc");
     history.push(`/librarypage?${qParmas.toString()}`);
   };
-  //* descending sort button #2.
-  //* The function retrieves the current search parameters from the browser's URL using the URLSearchParams object, sets the "sort" parameter to "asc" and updates the URL using the (history.push) method,
-  //* so the page will navigate to the (/librarypage) path with the updated parameters.
-  //* This allows the user to sort the games in the library in descending order.
+
   const handleSortDESCClick = () => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("sort", "desc");
     history.push(`/librarypage?${qParmas.toString()}`);
   };
 
-  //* this function takes an event as a parameter and sets the value of the "filterInput" state using the value of the target element of the event (event.target.value).
-  //* This is likely used as an onChange event handler for an input element, allowing the user to filter a list of games based on the input.
+
   const handleFindInputOnChange = (event) => {
     setFilterInput(event.target.value);
   };
 
-  //* this function takes in an id as a parameter.
-  //* Inside the function, it makes a deep copy of the current gameCardArr using JSON.parse(JSON.stringify(gameCardArr)), and then it uses the splice method to remove the element of the array with the corresponding id.
-  //* Then it makes a patch request to a specific endpoint ('/user/:id') with the updated wishList array.
-  //* It then dispatches an action to remove the game from the wishList in the application state and updates the gameCardArr state.
-  //* If there's any error in the process, it will show an error toast message.
+ 
   const handleGameCardDelete = (id) => async () => {
     try {
       const newArr = JSON.parse(JSON.stringify(gameCardArr));
@@ -202,7 +158,6 @@ const LibraryPage = () => {
     }
   };
 
-  //* The useEffect logs the gameCardArr state to the console, and it will be re-run every time the GameCardComponent changes.
   useEffect(() => {}, [GameCardComponent]);
 
   return (
--- src/pages/gameStore/GameStorePage.jsx
+++ src/pages/gameStore/GameStorePage.jsx
@@ -7,10 +7,7 @@ import "../gameStore/gameStorePage.scss";
 
 let initialGamesCardsArray = [];
 
-//* ("GameStorePage") uses the hooks "useState" and "useLocation" and "useHistory".
-//* The component has a state called "gameCardArr" which is initially set to the value of "initialGamesCardsArray".
-//* It also has two other states, "filterInput" and "categoryFilter", which are used to filter the game cards.
-//* The "useLocation" hook is used to access the current location object and the "useHistory" hook is used to navigate the user to different pages.
+
 const GameStorePage = () => {
   const [gameCardArr, setGameCardArr] = useState(initialGamesCardsArray);
   const [filterInput, setFilterInput] = useState("");
@@ -18,11 +15,7 @@ const GameStorePage = () => {
   const location = useLocation();
   const history = useHistory();
 
-  //* This useEffect hook is used to fetch all the game cards from the server when the component is first rendered.
-  //* It uses the axios to send a GET request to the ('/games/') endpoint and receives the game card data in the response.
-  //* The received data is then stored in the initialGamesCardsArray variable and the state gameCardArr is also updated with the received data.
-  //* The data received from the server is also logged to the console for debugging purposes.
-  //* This effect only runs once when the component is first rendered because of the empty dependency array.
+
   useEffect(() => {
     (async () => {
       try {
@@ -33,17 +26,7 @@ const GameStorePage = () => {
     })();
   }, []);
 
-  //* This useEffect hook is used to handle the filtering, sorting, and categorizing of the game cards that are displayed on the page.
-  //* It makes use of the location and initialGamesCardsArray state variables.
-  //* The hook runs every time the location changes, which occurs when the user makes changes to the filter, category, and sort parameters in the URL.
-  //* The hook first creates a new URLSearchParams object from the location.
-  //* search property, which is used to extract the filter, category, and sort parameters from the URL.
-  //* Then it creates a newFilteredArr variable which is used to store the filtered and sorted game cards.
-  //* If the category parameter is present in the URL, the hook filters the initialGamesCardsArray based on the category and assigns the result to the newFilteredArr.
-  //* If the filter parameter is present in the URL, the hook filters the newFilteredArr based on the filter and assigns the result to the newFilteredArr.
-  //* If the sort parameter is present in the URL, the hook sorts the newFilteredArr based on the sort parameter and assigns the result to the newFilteredArr.
-  //* Finally, if the newFilteredArr is not empty, it sets the gameCardArr state variable to newFilteredArr.
-  //* This causes the component to re-render and display the filtered and sorted game cards on the page.
+  
   useEffect(() => {
     const qParmas = new URLSearchParams(location.search);
     let newFilteredArr = undefined;
@@ -98,10 +81,7 @@ const GameStorePage = () => {
     if (newFilteredArr) setGameCardArr(newFilteredArr);
   }, [location, initialGamesCardsArray]);
 
-  //* The function checks if the key pressed is the Enter key.
-  //* If it is, the function creates a new URLSearchParams object using the current location's search property.
-  //* It then sets the "filter" parameter to the current value of the filterInput state.
-  //* It then updates the browser's URL by calling the push method on the history object, passing in a string with the new URL, including the updated query parameters.
+ 
   const handleInputKeyUp = (ev) => {
     if (ev.code === "Enter") {
       let qParmas = new URLSearchParams(location.search);
@@ -110,56 +90,37 @@ const GameStorePage = () => {
     }
   };
 
-  //* The function (handleCategoryFilter) is a callback that is called when the value of a select element is changed.
-  //* It updates the state categoryFilter with the new value selected by the user.
-  //* The event object is passed as an argument and the value of the select element is accessed using (ev.target.value).
-  //* This allows the component to keep track of the selected category and use it to filter the game cards accordingly.
+  
   const handleCategoryFilter = (ev) => {
     setCategoryFilter(ev.target.value);
   };
 
-  //* This useEffect function is used to handle the category filtering feature on the store page.
-  //* When the category filter is changed (by calling the handleCategoryFilter function), this useEffect function is triggered.
-  //* It creates a new URLSearchParams object, which is used to update the URL query parameters.
-  //* Specifically, it sets the "category" query parameter to the current value of categoryFilter.
-  //* Finally, it pushes the updated URL to the browser's history, which updates the URL without causing a full page reload.
-  //* This allows the user to see the filtered results without refreshing the page.
+  
   useEffect(() => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("category", categoryFilter);
     history.push(`/storepage?${qParmas.toString()}`);
   }, [categoryFilter]);
 
-  //* ascending sort button #1.
-  //* The function retrieves the current search parameters from the browser's URL using the URLSearchParams object,
-  //* sets the "sort" parameter to "asc" and updates the URL using the (history.push) method,
-  //* so the page will navigate to the (/librarypage) path with the updated parameters.
-  //* This allows the user to sort the games in the library in ascending order.
+  
   const handleSortASCClick = () => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("sort", "asc");
     history.push(`/storepage?${qParmas.toString()}`);
   };
-  //* descending sort button #2.
-  //* The function retrieves the current search parameters from the browser's URL using the URLSearchParams object,
-  //* sets the "sort" parameter to "asc" and updates the URL using the (history.push) method,
-  //* so the page will navigate to the (/librarypage) path with the updated parameters.
-  //* This allows the user to sort the games in the library in descending order.
+  
   const handleSortDESCClick = () => {
     let qParmas = new URLSearchParams(location.search);
     qParmas.set("sort", "desc");
     history.push(`/storepage?${qParmas.toString()}`);
   };
 
-  //* This function is handling the change event of an input element, search or filter input.
-  //* It takes the event as an argument and calls the setFilterInput function, which updates the filterInput state variable with the current value of the input element.
-  //* This allows the component to keep track of the current value of the input and use it for filtering or searching.
+  
   const handleFindInputOnChange = (event) => {
     setFilterInput(event.target.value);
   };
 
-  //* The handleGameCardDelete function is an async function that makes a DELETE request to the backend server to delete a game card with a specific ID.
-  //* It then updates the initialGamesCardsArray and gameCardArr state by removing the game card that has been deleted, so that the game card is no longer displayed.
+ 
   const handleGameCardDelete = async (id) => {
     try {
       const data = await axios.delete(`/games/${id}`);
@@ -191,7 +152,6 @@ const GameStorePage = () => {
     }
   };
 
-  //* The useEffect logs the gameCardArr state to the console, and it will be re-run every time the GameCardComponent changes.
   useEffect(() => {}, [GameCardComponent]);
 
   return (
